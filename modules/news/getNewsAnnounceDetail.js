956df016582e6b52ce49906c8057783e882acdc3
Yuan Guozheng <yuanguozheng@msn.cn>
2014-07-23 16:16:46 +0800

    Add News & Announce Detail API

--- modules/news/getNewsAnnounceDetail.js
+++ modules/news/getNewsAnnounceDetail.js
@@ -5,17 +5,32 @@ var request = require('request');
 var cheerio = require('cheerio');
 var iconv = require('iconv-lite');
 
-function getNewsAnnounceDetail(type, id, callback) {
+function getNewsAnnounceDetail(type, format, id, callback) {
     if (id == '' || id == undefined) {
         callback('Param Error');
         return;
     }
+
     var url;
     if (type == 'news') {
         url = 'http://222.24.3.10/news/article.asp?id=';
     } else if (type == 'announce') {
         url = 'http://222.24.3.10/announce/Article.asp?id=';
+    } else {
+        callback('Param Error');
+        return;
     }
+
+    var returnFormat;
+    if (format == 'text') {
+        returnFormat = 'text';
+    } else if (format == 'html') {
+        returnFormat = 'html';
+    } else {
+        callback('Param Error');
+        return;
+    }
+
     request
     (
         {
@@ -31,24 +46,37 @@ function getNewsAnnounceDetail(type, id, callback) {
 
             var temp = $('td[width=720]')[1].children[1].children;
             var contentTemp = temp;
-            console.log(temp[3].children[3].children[0].children[0].data);
+            var title = temp[3].children[3].children[0].children[0].data;
 
             temp = $('table[height=40]').children();
             var publisher = $(temp[0].children[3]).text().trim();
             var date = $(temp[1].children[3]).text().trim();
 
-            console.log(publisher + date);
+            var passage;
+            if (format == 'text') {
+                passage = $(contentTemp[7].children[3]).text();
+            } else if (format == 'html') {
+                $('table').removeAttr('width');
+                $('table').removeAttr('height');
+                $('td').removeAttr('width');
+                $('td').removeAttr('height');
+                $('font').removeAttr('style');
+                $('img').removeAttr('width');
+                $('img').removeAttr('height');
+                $('img').removeAttr('style');
+                passage = $(contentTemp[7].children[3]).html();
+            }
 
-            $('table').removeAttr('width');
-            $('table').removeAttr('height');
-            $('td').removeAttr('width');
-            $('td').removeAttr('height');
-            $('font').removeAttr('style');
+            var returnData =
+            {
+                Title: title,
+                Publisher: publisher,
+                Date: date,
+                Passage: passage
+            };
 
-            var rawContent = $(contentTemp[7].children[3]).html();
-            var textContent = $(contentTemp[7].children[3]).text();
-            console.log(rawContent);
-            console.log(textContent);
+            callback(returnData);
+            return;
         }
     );
 }
--- routes/news.js
+++ routes/news.js
@@ -8,38 +8,20 @@ var parsers = require('../modules/parsers');
 var getNewsAnnounceList = require('../modules/news/getNewsAnnounceList');
 var getNewAnnounceDetail = require('../modules/news/getNewsAnnounceDetail');
 
-function procListReq(type, req, res) {
-    var page = req.param('page', 1);
-    getNewsAnnounceList(type, page, function (result) {
-        parsers.resultProc(req, result, res);
-    });
-}
-
 router.use('/getList/:type/:page', function (req, res) {
     var page = req.param('page', 1);
-    var type = req.param('type','announce');
+    var type = req.param('type', 'announce');
     getNewsAnnounceList(type, page, function (result) {
         parsers.resultProc(req, result, res);
     });
 });
-/*
-router.use('/getAnnounceList', function (req, res) {
-    procListReq('announce', req, res);
-});
-
-router.use('/getNewsList/:page', function (req, res) {
-    procListReq('news', req, res);
-});
-
-router.use('/getNewsList', function (req, res) {
-    procListReq('news', req, res);
-});*/
 
 router.use('/getDetail/:type/:format/:id', function (req, res) {
     var id = req.param('id');
-    var type = req.param('type','announce');
-    getNewAnnounceDetail(type,id,function (result) {
-
+    var format = req.param('format', 'html');
+    var type = req.param('type', 'announce');
+    getNewAnnounceDetail(type, format, id, function (result) {
+        parsers.resultProc(req, result, res);
     });
 });
 
